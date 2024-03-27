970062c1a24b6cb8ca1a5b98379e702817362fdf
Lucas Fernandes <129906531+LucasFene@users.noreply.github.com>
2024-03-27 11:06:25 -0300

    Add files via upload

---
+++ lucas-aula-JAVA-SPRINGBOOT/src/main/java/com/example/lucasaulaJAVASPRINGBOOT/LucasAulaJavaSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.lucasaulaJAVASPRINGBOOT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LucasAulaJavaSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LucasAulaJavaSpringbootApplication.class, args);
+	}
+
+}
---
+++ lucas-aula-JAVA-SPRINGBOOT/src/test/java/com/example/lucasaulaJAVASPRINGBOOT/LucasAulaJavaSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lucasaulaJAVASPRINGBOOT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LucasAulaJavaSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
